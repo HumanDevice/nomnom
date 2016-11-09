00a672acabf018d483140de50782d54ff269e88f
Bizley <pawel@positive.codes>
2016-11-09 07:15:03 +0100

    tryc

--- commands/NomNomController.php
+++ commands/NomNomController.php
@@ -7,6 +7,7 @@ use app\models\Order;
 use app\models\Restaurant;
 use DateTime;
 use DateTimeZone;
+use Exception;
 use Yii;
 use yii\console\Controller;
 
@@ -20,26 +21,31 @@ class NomNomController extends Controller
      */
     public function actionStart()
     {
-        $order = new Order;
-        $order->stage = Order::STAGE_VOTE;
-        $order->stage_end = Yii::$app->formatter->asTimestamp(
-            DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d') . ' 11:00:00', new DateTimeZone(Yii::$app->timeZone))
-        );
-        
-        if (!$order->save()) {
-            Yii::error($order->errors);
-            Yii::$app->hipchat->send('(failed) Nie można otworzyć zamówienia!', 'red');
-            return Controller::EXIT_CODE_ERROR;
+        try {
+            $order = new Order;
+            $order->stage = Order::STAGE_VOTE;
+            $order->stage_end = Yii::$app->formatter->asTimestamp(
+                DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d') . ' 11:00:00', new DateTimeZone(Yii::$app->timeZone))
+            );
+
+            if (!$order->save()) {
+                Yii::error($order->errors);
+                throw new Exception('Nie można otworzyć zamówienia!');
+            }
+
+            Yii::$app->hipchat->send('@all NomNom otworzył nowe zamówienie!'
+                . "\n"
+                . 'Jedna restauracja jest już wybrana, ale jeśli masz smaka na coś innego, to przejdź do http://nomnom.projectown.net i zagłosuj na drugą.'
+                . "\n"
+                . 'Masz czas do 11:00 (10 minut) '
+                . HipChat::randomYay());
+
+            return Controller::EXIT_CODE_NORMAL;
+        } catch (Exception $exc) {
+            Yii::$app->hipchat->send('(failed) ' . $exc->getMessage(), 'red');
         }
-        
-        Yii::$app->hipchat->send('@all NomNom otworzył nowe zamówienie!'
-            . "\n"
-            . 'Jedna restauracja jest już wybrana, ale jeśli masz smaka na coś innego, to przejdź do http://nomnom.projectown.net i zagłosuj na drugą.'
-            . "\n"
-            . 'Masz czas do 11:00 (10 minut) '
-            . HipChat::randomYay());
-        
-        return Controller::EXIT_CODE_NORMAL;
+
+        return Controller::EXIT_CODE_ERROR;
     }
 
     /**
@@ -47,58 +53,61 @@ class NomNomController extends Controller
      */
     public function actionMeal()
     {
-        $order = Order::find()->where(['stage' => Order::STAGE_VOTE])->limit(1)->one();
-        if (!$order) {
-            return Controller::EXIT_CODE_NORMAL;
-        }
-        
-        $restaurantNames = [];
-        
-        $order->stage = Order::STAGE_MEAL;
-        $order->stage_end = Yii::$app->formatter->asTimestamp(
-            DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d') . ' 11:30:00', new DateTimeZone(Yii::$app->timeZone))
-        );
-        
-        $defaultRestaurant = Restaurant::findOne(Yii::$app->params['default_restaurant']);
-        if (empty($defaultRestaurant)) {
-            Yii::$app->hipchat->send('(failed) Nie można pobrać danych domyślnej restauracji!', 'red');
-            return Controller::EXIT_CODE_ERROR;
-        }
-        
-        $order->restaurant_id = $defaultRestaurant->id;
-        $restaurantNames[] = $defaultRestaurant->name;
-        
-        if (!empty($order->winner)) {
-            $chosenRestaurant = Restaurant::findOne($order->winner);
-            if (empty($chosenRestaurant)) {
-                Yii::$app->hipchat->send('(failed) Nie można pobrać danych drugiej restauracji!', 'red');
-                return Controller::EXIT_CODE_ERROR;
+        try {
+            $order = Order::find()->where(['stage' => Order::STAGE_VOTE])->limit(1)->one();
+            if (!$order) {
+                return Controller::EXIT_CODE_NORMAL;
             }
-            $order->restaurant2_id = $chosenRestaurant->id;
-            $restaurantNames[] = $chosenRestaurant->name;
+
+            $restaurantNames = [];
+
+            $order->stage = Order::STAGE_MEAL;
+            $order->stage_end = Yii::$app->formatter->asTimestamp(
+                DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d') . ' 11:30:00', new DateTimeZone(Yii::$app->timeZone))
+            );
+
+            $defaultRestaurant = Restaurant::findOne(Yii::$app->params['default_restaurant']);
+            if (empty($defaultRestaurant)) {
+                throw new Exception('Nie można pobrać danych domyślnej restauracji!');
+            }
+
+            $order->restaurant_id = $defaultRestaurant->id;
+            $restaurantNames[] = $defaultRestaurant->name;
+
+            if (!empty($order->winner)) {
+                $chosenRestaurant = Restaurant::findOne($order->winner);
+                if (empty($chosenRestaurant)) {
+                    throw new Exception('Nie można pobrać danych drugiej restauracji!');
+                }
+                $order->restaurant2_id = $chosenRestaurant->id;
+                $restaurantNames[] = $chosenRestaurant->name;
+            }
+
+            if (!$order->save()) {
+                Yii::error($order->errors);
+                throw new Exception('Nie można przejść do wyboru posiłku!');
+            }
+
+            $single = count($this->restaurantNames) == 1;
+
+            Yii::$app->hipchat->send('@all Naszym' . (!$single ? 'i' : '')
+                . ' dzisiejszym' . (!$single ? 'i' : '')
+                . ' kucharz' . (!$single ? 'ami' : 'em')
+                . ' ' . (!$single ? 'są' : 'jest')
+                . ' ' . $restaurantNames[0]
+                . (!$single ? ' i ' . $restaurantNames[1] : '')
+                . ".\n"
+                . 'Przejdź do http://nomnom.projectown.net i zapisz na co masz smaka (do 20zł).'
+                . "\n"
+                . 'Masz czas do 11:30 (pół godziny) '
+                . HipChat::randomYay());
+
+            return Controller::EXIT_CODE_NORMAL;
+        } catch (Exception $exc) {
+            Yii::$app->hipchat->send('(failed) ' . $exc->getMessage(), 'red');
         }
-        
-        if (!$order->save()) {
-            Yii::error($order->errors);
-            Yii::$app->hipchat->send('(failed) Nie można przejść do wyboru posiłku!', 'red');
-            return Controller::EXIT_CODE_ERROR;
-        }
-        
-        $single = count($this->restaurantNames) == 1;
-        
-        Yii::$app->hipchat->send('@all Naszym' . (!$single ? 'i' : '')
-            . ' dzisiejszym' . (!$single ? 'i' : '')
-            . ' kucharz' . (!$single ? 'ami' : 'em')
-            . ' ' . (!$single ? 'są' : 'jest')
-            . ' ' . $restaurantNames[0]
-            . (!$single ? ' i ' . $restaurantNames[1] : '')
-            . ".\n"
-            . 'Przejdź do http://nomnom.projectown.net i zapisz na co masz smaka (do 20zł).'
-            . "\n"
-            . 'Masz czas do 11:30 (pół godziny) '
-            . HipChat::randomYay());
-        
-        return Controller::EXIT_CODE_NORMAL;
+
+        return Controller::EXIT_CODE_ERROR;
     }
     
     /**
@@ -136,18 +145,23 @@ class NomNomController extends Controller
      */
     public function actionClose()
     {
-        $order = Order::find()->where(['!=', 'stage', Order::STAGE_CLOSE])->limit(1)->one();
-        if (!$order) {
+        try {
+            $order = Order::find()->where(['!=', 'stage', Order::STAGE_CLOSE])->limit(1)->one();
+            if (!$order) {
+                return Controller::EXIT_CODE_NORMAL;
+            }
+
+            $order->stage = Order::STAGE_CLOSE;
+            if (!$order->save()) {
+                Yii::error($order->errors);
+                throw new Exception('Nie można zamknąć zamówienia!');
+            }
+
             return Controller::EXIT_CODE_NORMAL;
+        } catch (Exception $exc) {
+            Yii::$app->hipchat->send('(failed) ' . $exc->getMessage(), 'red');
         }
-        
-        $order->stage = Order::STAGE_CLOSE;
-        if (!$order->save()) {
-            Yii::error($order->errors);
-            Yii::$app->hipchat->send('(failed) Nie można zamknąć zamówienia!', 'red');
-            return Controller::EXIT_CODE_ERROR;
-        }
-        
-        return Controller::EXIT_CODE_NORMAL;
+
+        return Controller::EXIT_CODE_ERROR;
     }
 }
