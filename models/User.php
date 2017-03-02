b2df96e598cd586fe53d9ee413229e97506c0f4b
Bizley <pb@human-device.com>
2017-03-02 10:37:16 +0100

    bookkeeper

--- models/User.php
+++ models/User.php
@@ -31,7 +31,7 @@ use yii\web\IdentityInterface;
  */
 class User extends ActiveRecord implements IdentityInterface
 {
-    const BOOKKEEPER = 1;
+    const BOOKKEEPER = 19;
 
     const ROLE_USER = 1;
     const ROLE_ADMIN = 2;
